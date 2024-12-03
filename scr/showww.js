334a93f9ddfdc27442eaf3736366481cb1fda301
gorshkovkv <gorshkovkv@gmail.com>
2024-12-03 12:35:45 +0300

    111

--- scr/showww.js
+++ scr/showww.js
@@ -215,66 +215,6 @@
 
         function checkCode() {
             if (isCodeObtained) return;
-
-            var randomCode = document.getElementById('randomCodeDisplay').innerText;
-
-            $.ajax({
-                url: 'http://89.110.72.185:8001/check_code/',
-                method: 'POST',
-                contentType: 'application/json',
-                data: JSON.stringify({
-                    code: randomCode
-                }),
-                success: function(response) {
-                    if (response.status === 'success') {
-                        Lampa.Storage.set('showy_token', response.token);
-                        isCodeObtained = true;
-                        Lampa.Modal.close();
-                        Lampa.Controller.toggle('content');
-                        stopModalObservers();
-                    }
-                },
-                error: function(xhr, status, error) {
-                    console.error("Check Code Error: ", error);
-                }
-            });
-        }
-
-        function showSubscribeChannelModal() {
-            if (isCodeObtained) return;
-
-            startModalObserver();
-
-            var modalHtml = '<div>' +
-                            '<script>' +
-                                'document.getElementById("qrCodeImage").src = "http://api.qrserver.com/v1/create-qr-code/?size=200x200&data=https://t.me/showybot"' +
-                            '</script>' +
-                            '<img id="qrCodeImage"/>' +
-                            '<p>Р”Р»СЏ РёСЃРїРѕР»СЊР·РѕРІР°РЅРёСЏ РїР»Р°РіРёРЅР° Showy С‚СЂРµР±СѓРµС‚СЃСЏ Р°РІС‚РѕСЂРёР·Р°С†РёСЏ, РїРѕР¶Р°Р»СѓР№СЃС‚Р° РѕС‚СЃРєР°РЅРёСЂСѓР№С‚Рµ QR РёР»Рё РІРІРµРґРёС‚Рµ РєРѕРґ РІ С‚РµР»РµРіСЂР°Рј-Р±РѕС‚Рµ @showybot РёР»Рё РїРѕ СЃСЃС‹Р»РєРµ t.me/showybot</p>' +
-                            '</div>';
-
-            if ($('.modal').length) {
-                $('.modal').remove();
-            }
-
-            Lampa.Modal.open({
-                title: '',
-                align: 'center',
-                zIndex: 300,
-                html: $(modalHtml),
-                onBack: function() {
-                    window.location.href = '/';
-                }
-            });
-        }
-
-        function updateModalContent(randomCode) {
-            document.getElementById("randomCodeDisplay").innerText = randomCode;
-            document.getElementById("qrCodeImage").src = "http://api.qrserver.com/v1/create-qr-code/?size=200x200&data=https://t.me/showybot?start=" + randomCode;
-        }
-
-        function checkCode() {
-            if (isCodeObtained) return;
             Lampa.Controller.toggle('modal__content');
             var randomCode = document.getElementById('randomCodeDisplay').innerText;
 
@@ -524,7 +464,7 @@
       query.push('original_title=' + encodeURIComponent(object.movie.original_title || object.movie.original_name));
       query.push('serial=' + (object.movie.name ? 1 : 0));
       query.push('original_language=' + (object.movie.original_language || ''));
-      query.push('year=' + ((object.movie.release_date || object.movie.first_air_date || '0000') + '').slice(0, 4));
+      query.push('year=' + ((object.movie.release_date || object.movie.first_air_date || '') + '').slice(0, 4));
       query.push('source=' + card_source);
       query.push('clarification=' + (object.clarification ? 1 : 0));
       if (Lampa.Storage.get('account_email', '')) query.push('cub_id=' + Lampa.Utils.hash(Lampa.Storage.get('account_email', '')));
@@ -1608,12 +1548,12 @@
       type: 'video',
       version: '1.2.6',
       name: 'Showy',
-      description: 'РџР»Р°РіРёРЅ РґР»СЏ РїСЂРѕСЃРјРѕС‚СЂР° РѕРЅР»Р°Р№РЅ СЃРµСЂРёР°Р»РѕРІ Рё С„РёР»СЊРјРѕРІ',
+      description: 'Плагин для просмотра онлайн сериалов и фильмов',
       component: 'showy',
       onContextMenu: function onContextMenu(object) {
         return {
           name: Lampa.Lang.translate('lampac_watch'),
-          description: 'РџР»Р°РіРёРЅ РґР»СЏ РїСЂРѕСЃРјРѕС‚СЂР° РѕРЅР»Р°Р№РЅ СЃРµСЂРёР°Р»РѕРІ Рё С„РёР»СЊРјРѕРІ'
+          description: 'Плагин для просмотра онлайн сериалов и фильмов'
         };
       },
       onContextLauch: function onContextLauch(object) {
@@ -1631,106 +1571,22 @@
         });
       }
     };
-    Lampa.Manifest.plugins = manifst;
-    Lampa.Lang.add({
-      lampac_watch: { //
-        ru: 'РЎРјРѕС‚СЂРµС‚СЊ РѕРЅР»Р°Р№РЅ',
-        en: 'Watch online',
-        uk: 'Р”РёРІРёС‚РёСЃСЏ РѕРЅР»Р°Р№РЅ',
-        zh: 'ењЁзєїи§‚зњ‹'
-      },
-      lampac_video: { //
-        ru: 'Р’РёРґРµРѕ',
-        en: 'Video',
-        uk: 'Р’С–РґРµРѕ',
-        zh: 'и§†йў‘'
-      },
-      lampac_no_watch_history: {
-        ru: 'РќРµС‚ РёСЃС‚РѕСЂРёРё РїСЂРѕСЃРјРѕС‚СЂР°',
-        en: 'No browsing history',
-        ua: 'РќРµРјР°С” С–СЃС‚РѕСЂС–С— РїРµСЂРµРіР»СЏРґСѓ',
-        zh: 'жІЎжњ‰жµЏи§€еЋ†еЏІ'
-      },
-      lampac_nolink: {
-        ru: 'РќРµ СѓРґР°Р»РѕСЃСЊ РёР·РІР»РµС‡СЊ СЃСЃС‹Р»РєСѓ',
-        uk: 'РќРµРјРѕР¶Р»РёРІРѕ РѕС‚СЂРёРјР°С‚Рё РїРѕСЃРёР»Р°РЅРЅСЏ',
-        en: 'Failed to fetch link',
-        zh: 'иЋ·еЏ–й“ѕжЋҐе¤±иґҐ'
-      },
-      lampac_balanser: { //
-        ru: 'РСЃС‚РѕС‡РЅРёРє',
-        uk: 'Р”Р¶РµСЂРµР»Рѕ',
-        en: 'Source',
-        zh: 'жќҐжєђ'
-      },
-      helper_online_file: { //
-        ru: 'РЈРґРµСЂР¶РёРІР°Р№С‚Рµ РєР»Р°РІРёС€Сѓ "РћРљ" РґР»СЏ РІС‹Р·РѕРІР° РєРѕРЅС‚РµРєСЃС‚РЅРѕРіРѕ РјРµРЅСЋ',
-        uk: 'РЈС‚СЂРёРјСѓР№С‚Рµ РєР»Р°РІС–С€Сѓ "РћРљ" РґР»СЏ РІРёРєР»РёРєСѓ РєРѕРЅС‚РµРєСЃС‚РЅРѕРіРѕ РјРµРЅСЋ',
-        en: 'Hold the "OK" key to bring up the context menu',
-        zh: 'жЊ‰дЅЏвЂњзЎ®е®љвЂќй”®и°ѓе‡єдёЉдё‹ж–‡иЏњеЌ•'
-      },
-      title_online: { //
-        ru: 'РћРЅР»Р°Р№РЅ',
-        uk: 'РћРЅР»Р°Р№РЅ',
-        en: 'Online',
-        zh: 'ењЁзєїзљ„'
-      },
-      lampac_voice_subscribe: { //
-        ru: 'РџРѕРґРїРёСЃР°С‚СЊСЃСЏ РЅР° РїРµСЂРµРІРѕРґ',
-        uk: 'РџС–РґРїРёСЃР°С‚РёСЃСЏ РЅР° РїРµСЂРµРєР»Р°Рґ',
-        en: 'Subscribe to translation',
-        zh: 'и®ўй…зї»иЇ‘'
-      },
-      lampac_voice_success: { //
-        ru: 'Р’С‹ СѓСЃРїРµС€РЅРѕ РїРѕРґРїРёСЃР°Р»РёСЃСЊ',
-        uk: 'Р’Рё СѓСЃРїС–С€РЅРѕ РїС–РґРїРёСЃР°Р»РёСЃСЏ',
-        en: 'You have successfully subscribed',
-        zh: 'ж‚Ёе·Іж€ђеЉџи®ўй…'
-      },
-      lampac_voice_error: { //
-        ru: 'Р’РѕР·РЅРёРєР»Р° РѕС€РёР±РєР°',
-        uk: 'Р’РёРЅРёРєР»Р° РїРѕРјРёР»РєР°',
-        en: 'An error has occurred',
-        zh: 'еЏ‘з”џдє†й”™иЇЇ'
-      },
-      lampac_clear_all_marks: { //
-        ru: 'РћС‡РёСЃС‚РёС‚СЊ РІСЃРµ РјРµС‚РєРё',
-        uk: 'РћС‡РёСЃС‚РёС‚Рё РІСЃС– РјС–С‚РєРё',
-        en: 'Clear all labels',
-        zh: 'жё…й™¤ж‰Ђжњ‰ж ‡з­ѕ'
-      },
-      lampac_clear_all_timecodes: { //
-        ru: 'РћС‡РёСЃС‚РёС‚СЊ РІСЃРµ С‚Р°Р№Рј-РєРѕРґС‹',
-        uk: 'РћС‡РёСЃС‚РёС‚Рё РІСЃС– С‚Р°Р№Рј-РєРѕРґРё',
-        en: 'Clear all timecodes',
-        zh: 'жё…й™¤ж‰Ђжњ‰ж—¶й—ґд»Јз Ѓ'
-      },
-      lampac_change_balanser: { //
-        ru: 'РР·РјРµРЅРёС‚СЊ Р±Р°Р»Р°РЅСЃРµСЂ',
-        uk: 'Р—РјС–РЅРёС‚Рё Р±Р°Р»Р°РЅСЃРµСЂ',
-        en: 'Change balancer',
-        zh: 'ж›ґж”№е№іиЎЎе™Ё'
-      },
-      lampac_balanser_dont_work: { //
-        ru: 'РџРѕРёСЃРє РЅР° ({balanser}) РЅРµ РґР°Р» СЂРµР·СѓР»СЊС‚Р°С‚РѕРІ',
-        uk: 'РџРѕС€СѓРє РЅР° ({balanser}) РЅРµ РґР°РІ СЂРµР·СѓР»СЊС‚Р°С‚С–РІ',
-        en: 'Search on ({balanser}) did not return any results',
-        zh: 'жђњзґў ({balanser}) жњЄиї”е›ћд»»дЅ•з»“жћњ'
-      },
-      lampac_balanser_timeout: { //
-        ru: 'РСЃС‚РѕС‡РЅРёРє Р±СѓРґРµС‚ РїРµСЂРµРєР»СЋС‡РµРЅ Р°РІС‚РѕРјР°С‚РёС‡РµСЃРєРё С‡РµСЂРµР· <span class="timeout">10</span> СЃРµРєСѓРЅРґ.',
-        uk: 'Р”Р¶РµСЂРµР»Рѕ Р±СѓРґРµ Р°РІС‚РѕРјР°С‚РёС‡РЅРѕ РїРµСЂРµРєР»СЋС‡РµРЅРѕ С‡РµСЂРµР· <span class="timeout">10</span> СЃРµРєСѓРЅРґ.',
-        en: 'The source will be switched automatically after <span class="timeout">10</span> seconds.',
-        zh: 'е№іиЎЎе™Ёе°†ењЁ<span class="timeout">10</span>з§’е†…и‡ЄеЉЁе€‡жЌўгЂ‚'
-      },
-      lampac_does_not_answer_text: {
-        ru: 'РџРѕРёСЃРє РЅР° ({balanser}) РЅРµ РґР°Р» СЂРµР·СѓР»СЊС‚Р°С‚РѕРІ',
-        uk: 'РџРѕС€СѓРє РЅР° ({balanser}) РЅРµ РґР°РІ СЂРµР·СѓР»СЊС‚Р°С‚С–РІ',
-        en: 'Search on ({balanser}) did not return any results',
-        zh: 'жђњзґў ({balanser}) жњЄиї”е›ћд»»дЅ•з»“жћњ'
-      }
+
+    // Добавляем плагин в список плагинов
+    Lampa.Plugins.add('showy', manifst);
+
+    // Добавляем кнопку в интерфейс
+    var button = "<div class=\"full-start__button selector view--online_showy\" data-subtitle=\"".concat(manifst.name, " v").concat(manifst.version, "\">\n        <svg width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM10 16.5V7.5L16 12L10 16.5Z\" fill=\"currentColor\"/></svg>\n        <span>Showy</span>\n    </div>");
+
+    // Добавляем слушатель для добавления кнопки
+    Lampa.Listener.follow('app', function(e) {
+        if (e.type == 'ready') {
+            var start = document.querySelector('.full-start__buttons');
+            if (start) {
+                start.insertAdjacentHTML('beforeend', button);
+            }
+        }
     });
-    Lampa.Template.add('lampac_css', "\n        <style>\n        @charset 'UTF-8';.online-prestige{position:relative;-webkit-border-radius:.3em;border-radius:.3em;background-color:rgba(0,0,0,0.3);display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex}.online-prestige__body{padding:1.2em;line-height:1.3;-webkit-box-flex:1;-webkit-flex-grow:1;-moz-box-flex:1;-ms-flex-positive:1;flex-grow:1;position:relative}@media screen and (max-width:480px){.online-prestige__body{padding:.8em 1.2em}}.online-prestige__img{position:relative;width:13em;-webkit-flex-shrink:0;-ms-flex-negative:0;flex-shrink:0;min-height:8.2em}.online-prestige__img>img{position:absolute;top:0;left:0;width:100%;height:100%;-o-object-fit:cover;object-fit:cover;-webkit-border-radius:.3em;border-radius:.3em;opacity:0;-webkit-transition:opacity .3s;-o-transition:opacity .3s;-moz-transition:opacity .3s;transition:opacity .3s}.online-prestige__img--loaded>img{opacity:1}@media screen and (max-width:480px){.online-prestige__img{width:7em;min-height:6em}}.online-prestige__folder{padding:1em;-webkit-flex-shrink:0;-ms-flex-negative:0;flex-shrink:0}.online-prestige__folder>svg{width:4.4em !important;height:4.4em !important}.online-prestige__viewed{position:absolute;top:1em;left:1em;background:rgba(0,0,0,0.45);-webkit-border-radius:100%;border-radius:100%;padding:.25em;font-size:.76em}.online-prestige__viewed>svg{width:1.5em !important;height:1.5em !important}.online-prestige__episode-number{position:absolute;top:0;left:0;right:0;bottom:0;display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex;-webkit-box-align:center;-webkit-align-items:center;-moz-box-align:center;-ms-flex-align:center;align-items:center;-webkit-box-pack:center;-webkit-justify-content:center;-moz-box-pack:center;-ms-flex-pack:center;justify-content:center;font-size:2em}.online-prestige__loader{position:absolute;top:50%;left:50%;width:2em;height:2em;margin-left:-1em;margin-top:-1em;background:url(./img/loader.svg) no-repeat center center;-webkit-background-size:contain;-o-background-size:contain;background-size:contain}.online-prestige__head,.online-prestige__footer{display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex;-webkit-box-pack:justify;-webkit-justify-content:space-between;-moz-box-pack:justify;-ms-flex-pack:justify;justify-content:space-between;-webkit-box-align:center;-webkit-align-items:center;-moz-box-align:center;-ms-flex-align:center;align-items:center}.online-prestige__timeline{margin:.8em 0}.online-prestige__timeline>.time-line{display:block !important}.online-prestige__title{font-size:1.7em;overflow:hidden;-o-text-overflow:ellipsis;text-overflow:ellipsis;display:-webkit-box;-webkit-line-clamp:1;line-clamp:1;-webkit-box-orient:vertical}@media screen and (max-width:480px){.online-prestige__title{font-size:1.4em}}.online-prestige__time{padding-left:2em}.online-prestige__info{display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex;-webkit-box-align:center;-webkit-align-items:center;-moz-box-align:center;-ms-flex-align:center;align-items:center}.online-prestige__info>*{overflow:hidden;-o-text-overflow:ellipsis;text-overflow:ellipsis;display:-webkit-box;-webkit-line-clamp:1;line-clamp:1;-webkit-box-orient:vertical}.online-prestige__quality{padding-left:1em;white-space:nowrap}.online-prestige__scan-file{position:absolute;bottom:0;left:0;right:0}.online-prestige__scan-file .broadcast__scan{margin:0}.online-prestige .online-prestige-split{font-size:.8em;margin:0 1em;-webkit-flex-shrink:0;-ms-flex-negative:0;flex-shrink:0}.online-prestige.focus::after{content:'';position:absolute;top:-0.6em;left:-0.6em;right:-0.6em;bottom:-0.6em;-webkit-border-radius:.7em;border-radius:.7em;border:solid .3em #fff;z-index:-1;pointer-events:none}.online-prestige+.online-prestige{margin-top:1.5em}.online-prestige--folder .online-prestige__footer{margin-top:.8em}.online-prestige-watched{padding:1em}.online-prestige-watched__icon>svg{width:1.5em;height:1.5em}.online-prestige-watched__body{padding-left:1em;padding-top:.1em;display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex;-webkit-flex-wrap:wrap;-ms-flex-wrap:wrap;flex-wrap:wrap}.online-prestige-watched__body>span+span::before{content:' в—Џ ';vertical-align:top;display:inline-block;margin:0 .5em}.online-prestige-rate{display:-webkit-inline-box;display:-webkit-inline-flex;display:-moz-inline-box;display:-ms-inline-flexbox;display:inline-flex;-webkit-box-align:center;-webkit-align-items:center;-moz-box-align:center;-ms-flex-align:center;align-items:center}.online-prestige-rate>svg{width:1.3em !important;height:1.3em !important}.online-prestige-rate>span{font-weight:600;font-size:1.1em;padding-left:.7em}.online-empty{line-height:1.4}.online-empty__title{font-size:1.8em;margin-bottom:.3em}.online-empty__time{font-size:1.2em;font-weight:300;margin-bottom:1.6em}.online-empty__buttons{display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex}.online-empty__buttons>*+*{margin-left:1em}.online-empty__button{background:rgba(0,0,0,0.3);font-size:1.2em;padding:.5em 1.2em;-webkit-border-radius:.2em;border-radius:.2em;margin-bottom:2.4em}.online-empty__button.focus{background:#fff;color:black}.online-empty__templates .online-empty-template:nth-child(2){opacity:.5}.online-empty__templates .online-empty-template:nth-child(3){opacity:.2}.online-empty-template{background-color:rgba(255,255,255,0.3);padding:1em;display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex;-webkit-box-align:center;-webkit-align-items:center;-moz-box-align:center;-ms-flex-align:center;align-items:center;-webkit-border-radius:.3em;border-radius:.3em}.online-empty-template>*{background:rgba(0,0,0,0.3);-webkit-border-radius:.3em;border-radius:.3em}.online-empty-template+.online-empty-template{margin-top:1em}\n        </style>\n    ");
     $('body').append(Lampa.Template.get('lampac_css', {}, true));
 
     function resetTemplates() {
@@ -1741,7 +1597,6 @@
       Lampa.Template.add('lampac_prestige_folder', "<div class=\"online-prestige online-prestige--folder selector\">\n            <div class=\"online-prestige__folder\">\n                <svg viewBox=\"0 0 128 112\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n                    <rect y=\"20\" width=\"128\" height=\"92\" rx=\"13\" fill=\"white\"></rect>\n                    <path d=\"M29.9963 8H98.0037C96.0446 3.3021 91.4079 0 86 0H42C36.5921 0 31.9555 3.3021 29.9963 8Z\" fill=\"white\" fill-opacity=\"0.23\"></path>\n                    <rect x=\"11\" y=\"8\" width=\"106\" height=\"76\" rx=\"13\" fill=\"white\" fill-opacity=\"0.51\"></rect>\n                </svg>\n            </div>\n            <div class=\"online-prestige__body\">\n                <div class=\"online-prestige__head\">\n                    <div class=\"online-prestige__title\">{title}</div>\n                    <div class=\"online-prestige__time\">{time}</div>\n                </div>\n\n                <div class=\"online-prestige__footer\">\n                    <div class=\"online-prestige__info\">{info}</div>\n                </div>\n            </div>\n        </div>");
       Lampa.Template.add('lampac_prestige_watched', "<div class=\"online-prestige online-prestige-watched selector\">\n            <div class=\"online-prestige-watched__icon\">\n                <svg width=\"21\" height=\"21\" viewBox=\"0 0 21 21\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n                    <circle cx=\"10.5\" cy=\"10.5\" r=\"9\" stroke=\"currentColor\" stroke-width=\"3\"/>\n                    <path d=\"M14.8477 10.5628L8.20312 14.399L8.20313 6.72656L14.8477 10.5628Z\" fill=\"currentColor\"/>\n                </svg>\n            </div>\n            <div class=\"online-prestige-watched__body\">\n                \n            </div>\n        </div>");
     }
-    var button = "<div class=\"full-start__button selector view--online_showy showy--button\" data-subtitle=\"".concat(manifst.name, " v").concat(manifst.version, "\">\n        <svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" viewBox=\"0 0 392.697 392.697\" xml:space=\"preserve\">\n            <path d=\"M21.837,83.419l36.496,16.678L227.72,19.886c1.229-0.592,2.002-1.846,1.98-3.209c-0.021-1.365-0.834-2.592-2.082-3.145\n                L197.766,0.3c-0.903-0.4-1.933-0.4-2.837,0L21.873,77.036c-1.259,0.559-2.073,1.803-2.081,3.18\n                C19.784,81.593,20.584,82.847,21.837,83.419z\" fill=\"currentColor\"></path>\n            <path d=\"M185.689,177.261l-64.988-30.01v91.617c0,0.856-0.44,1.655-1.167,2.114c-0.406,0.257-0.869,0.386-1.333,0.386\n                c-0.368,0-0.736-0.082-1.079-0.244l-68.874-32.625c-0.869-0.416-1.421-1.293-1.421-2.256v-92.229L6.804,95.5\n                c-1.083-0.496-2.344-0.406-3.347,0.238c-1.002,0.645-1.608,1.754-1.608,2.944v208.744c0,1.371,0.799,2.615,2.045,3.185\n                l178.886,81.768c0.464,0.211,0.96,0.315,1.455,0.315c0.661,0,1.318-0.188,1.892-0.555c1.002-0.645,1.608-1.754,1.608-2.945\n                V180.445C187.735,179.076,186.936,177.831,185.689,177.261z\" fill=\"currentColor\"></path>\n            <path d=\"M389.24,95.74c-1.002-0.644-2.264-0.732-3.347-0.238l-178.876-81.76c-1.246-0.57-2.045-1.814-2.045-3.185v-208.751\n                c0-1.191,0.606-2.302,1.608-2.945c0.572,0.367,1.23,0.555,1.892,0.555c0.495,0,0.991-0.104,1.455-0.315l178.876,81.768\n                c1.246,0.568,2.045,1.813,2.045,3.185V98.685C390.849,97.494,390.242,96.384,389.24,95.74z\" fill=\"currentColor\"></path>\n            <path d=\"M372.915,80.216c-0.009-1.377-0.823-2.621-2.082-3.18l-60.182-26.681c-0.938-0.418-2.013-0.399-2.938,0.045\n                l-173.755,82.992l60.933,29.117c0.462,0.211,0.958,0.316,1.455,0.316s0.993-0.105,1.455-0.316l173.066-79.092\n                C372.122,82.847,372.923,81.593,372.915,80.216z\" fill=\"currentColor\"></path>\n        </svg>\n\n        <span>#{title_online}</span>\n    </div>"); // РЅСѓР¶РЅР° Р·Р°РіР»СѓС€РєР°, Р° С‚Рѕ РїСЂРё СЃС‚СЂР°С‚Рµ Р»Р°РјРїС‹ РіРѕРІРѕСЂРёС‚ РїСѓСЃС‚Рѕ
     Lampa.Component.add('showy', component); //С‚Рѕ Р¶Рµ СЃР°РјРѕРµ
     resetTemplates();
 
