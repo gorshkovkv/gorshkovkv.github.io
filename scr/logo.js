b565b10d97194fc7d498868d281185a7519e18c7
gorshkovkv <gorshkovkv@gmail.com>
2024-12-04 12:09:55 +0300

    11111

--- scr/logo.js
+++ scr/logo.js
@@ -23,22 +23,44 @@
         Lampa.Listener.follow("full", function(e) {
             if (e.type == "complite" && Lampa.Storage.get("logo_glav") != "1") {
                 var movie = e.data.movie;
-                var url = Lampa.TMDB.api((movie.name ? "tv" : "movie") + "/" + movie.id + "/images?api_key=" + Lampa.TMDB.key() + "&language=" + Lampa.Storage.get("language"));
                 
-                $.get(url, function(resp) {
-                    if (resp.logos && resp.logos[0]) {
-                        var path = resp.logos[0].file_path;
-                        if (path != "") {
-                            var imgElement = $('<img style="margin-top: 5px;max-height: 125px;" src="' + Lampa.TMDB.image("/t/p/w300" + path.replace(".svg", ".png")) + '" />');
-                            imgElement.on('error', function() {
-                                $(".full-start-new__title").html(movie.title || movie.name);
-                            });
-                            $(".full-start-new__title").html(imgElement);
+                async function tryGetLogo(lang) {
+                    var url = Lampa.TMDB.api((movie.name ? "tv" : "movie") + "/" + movie.id + "/images?api_key=" + Lampa.TMDB.key() + "&language=" + lang);
+                    try {
+                        const resp = await $.get(url);
+                        if (resp.logos && resp.logos[0]) {
+                            return resp.logos[0].file_path;
                         }
+                    } catch (e) {}
+                    return null;
+                }
+
+                async function findLogo() {
+                    // Пробуем найти на текущем языке
+                    let path = await tryGetLogo(Lampa.Storage.get("language"));
+                    
+                    // Если нет на текущем языке, пробуем английский
+                    if (!path) {
+                        path = await tryGetLogo("en");
+                    }
+                    
+                    // Если нет на английском, пробуем язык оригинала
+                    if (!path) {
+                        path = await tryGetLogo(null);
+                    }
+
+                    if (path) {
+                        var imgElement = $('<img style="margin-top: 5px;max-height: 125px;" src="' + Lampa.TMDB.image("/t/p/w300" + path.replace(".svg", ".png")) + '" />');
+                        imgElement.on('error', function() {
+                            $(".full-start-new__title").html(movie.title || movie.name);
+                        });
+                        $(".full-start-new__title").html(imgElement);
                     } else {
                         $(".full-start-new__title").html(movie.title || movie.name);
                     }
-                });
+                }
+
+                findLogo();
             }
         });
     }
