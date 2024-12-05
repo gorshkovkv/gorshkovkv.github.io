c8f41f980ba79e84a6c1e385d034cba832009043
gorshkovkv <gorshkovkv@gmail.com>
2024-12-05 22:31:48 +0300

    1111

--- scr/logo.js
+++ scr/logo.js
@@ -210,9 +210,8 @@
         // Переопределяем метод получения пути к изображению
         var originalImageFunction = Lampa.TMDB.image;
         Lampa.TMDB.image = function(url) {
-            // Если включена настройка высокого качества, заменяем пути
+            // Если включена настройка высокого качества, заменяем путь
             if (Lampa.Storage.field('logo_high_quality')) {
-                url = url.replace('t/p/w780', 't/p/original');
                 url = url.replace('t/p/w1280', 't/p/original');
             }
             return originalImageFunction(url);
