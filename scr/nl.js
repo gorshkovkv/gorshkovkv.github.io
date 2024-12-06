f2227ad5a1a702f80be85ac298db5e39d8bcb3ac
gorshkovkv <gorshkovkv@gmail.com>
2024-12-07 02:33:09 +0300

    1111

--- scr/nl.js
+++ scr/nl.js
@@ -24,35 +24,24 @@
             const style = `
                 <style id="nav-landscape-style">
                     @media screen and (orientation: landscape) {
-                        body:not(.true--mobile) .navigation-bar {
+                        .navigation-bar {
                             left: 25% !important;
                             right: 25% !important;
                             top: auto !important;
                             bottom: 2em !important;
                             padding: 0 !important;
-                            background: none !important;
-                            box-shadow: none !important;
                         }
                         
-                        body:not(.true--mobile) .navigation-bar .navigation-bar__body {
+                        .navigation-bar .navigation-bar__body {
                             flex-direction: row !important;
                             width: 100% !important;
                             backdrop-filter: none !important;
                             -webkit-backdrop-filter: none !important;
                             background-color: rgba(0,0,0,0.3) !important;
-                            box-shadow: 0 0 1em rgba(0,0,0,0.3) !important;
                         }
 
-                        body.true--mobile .navigation-bar {
-                            width: 5em;
-                            right: 0 !important;
-                            background: none !important;
-                            box-shadow: none !important;
-                        }
-
-                        body.true--mobile .navigation-bar .navigation-bar__body {
-                            background-color: rgba(0,0,0,0.3) !important;
-                            box-shadow: 0 0 1em rgba(0,0,0,0.3) !important;
+                        .wrap__content.layer--height.layer--width {
+                            padding-bottom: 8em !important;
                         }
                     }
                 </style>
