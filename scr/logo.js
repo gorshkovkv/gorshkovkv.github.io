37d3c4eb5191f84f606114524acc0d41f5453668
gorshkovkv <gorshkovkv@gmail.com>
2024-12-06 23:00:35 +0300

    1111

--- scr/logo.js
+++ scr/logo.js
@@ -42,20 +42,6 @@
         }
     });
 
-    // Добавляем настройку для навигационной панели
-    Lampa.SettingsApi.addParam({
-        component: "interface",
-        param: {
-            name: "logo_nav_right",
-            type: "trigger",
-            default: false
-        },
-        field: {
-            name: "Навигация справа в ландшафте",
-            description: "Отображает навигационную панель справа в ландшафтном режиме"
-        }
-    });
-
     // Добавляем настройку для качества изображений
     Lampa.SettingsApi.addParam({
         component: "interface",
@@ -130,36 +116,6 @@
                         }
                         .full-start-new__buttons {
                             order: 7 !important;
-                        }
-                    }
-                </style>
-            `);
-        }
-
-        if (!$('#logo-nav-style').length && Lampa.Storage.get('logo_nav_right')) {
-            $('head').append(`
-                <style id="logo-nav-style">
-                    @media screen and (orientation: landscape) {
-                        body[data-nav-right="true"] .navigation-bar {
-                            top: 0;
-                            left: auto;
-                            right: 0;
-                            display: -webkit-box;
-                            display: -webkit-flex;
-                            display: -moz-box;
-                            display: -ms-flexbox;
-                            display: flex;
-                            padding: 0.5em;
-                            padding-left: 0;
-                        }
-                        body[data-nav-right="true"] .navigation-bar .navigation-bar__body {
-                            -webkit-box-orient: vertical;
-                            -webkit-box-direction: normal;
-                            -webkit-flex-direction: column;
-                            -moz-box-orient: vertical;
-                            -moz-box-direction: normal;
-                            -ms-flex-direction: column;
-                            flex-direction: column;
                         }
                     }
                 </style>
@@ -255,15 +211,12 @@
                 over: true
             };
             
-            if (Lampa.Storage.get('logo_nav_right')) {
-                // Если включена правая навигация, создаем новый скролл
-                mainScroll = new Lampa.Scroll(scrollConfig);
-                
-                // Находим основной контейнер
-                let mainContainer = $('.activity--active .activity__body');
-                if (mainContainer.length) {
-                    mainScroll.render().appendTo(mainContainer);
-                }
+            mainScroll = new Lampa.Scroll(scrollConfig);
+            
+            // Находим основной контейнер
+            let mainContainer = $('.activity--active .activity__body');
+            if (mainContainer.length) {
+                mainScroll.render().appendTo(mainContainer);
             }
         }
         
@@ -281,20 +234,11 @@
         
         // Обновляем при открытии настроек
         Lampa.Settings.listener.follow('open', initScroll);
-        
-        // Следим за изменением настройки навигационной панели
-        Lampa.Storage.listener.follow('change', function (event) {
-            if (event.name == 'logo_nav_right') {
-                $('body').attr('data-nav-right', event.value);
-                initScroll();
-            }
-        });
-        
-        // Устанавливаем начальное значение
-        $('body').attr('data-nav-right', Lampa.Storage.get('logo_nav_right'));
 
-        // Переопределяем метод получения пути к изображению
+        // Сохраняем оригинальную функцию
         var originalImageFunction = Lampa.TMDB.image;
+
+        // Переопределяем функцию для изменения качества изображений
         Lampa.TMDB.image = function(url) {
             // Если включена настройка высокого качества, заменяем путь
             if (Lampa.Storage.field('logo_high_quality')) {
