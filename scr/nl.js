4df0057d5a0d71aa176b6dfd17efa39836acc901
gorshkovkv <gorshkovkv@gmail.com>
2024-12-07 03:43:31 +0300

    1111

--- scr/nl.js
+++ scr/nl.js
@@ -52,7 +52,7 @@
                         }
 
                         .explorer .scroll__content {
-                            min-height: 95vh !important;
+                            padding-bottom: 8em !important;
                         }
                     }
                 </style>
