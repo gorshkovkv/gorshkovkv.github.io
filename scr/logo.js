488e1132c5fb06b5098200d9306fbb84ae6d924d
gorshkovkv <gorshkovkv@gmail.com>
2024-12-07 05:02:25 +0300

    1111

--- scr/logo.js
+++ scr/logo.js
@@ -358,7 +358,7 @@
                     // Если включена настройка логотипов, пробуем найти и отобразить логотип
                     if (Lampa.Storage.get("logo_glav")) {
                         // Создаем контейнер для логотипа
-                        var imgElement = $('<img style="max-height: 2.5em;" src="' + Lampa.TMDB.image("/t/p/w500" + path.replace(".svg", ".png")) + '" />');
+                        var imgElement = $('<img style="max-height: 2em;" src="' + Lampa.TMDB.image("/t/p/w500" + path.replace(".svg", ".png")) + '" />');
                         //var imgElement = $('<img style="margin-top: 5px; margin-left: 0; width: auto; height: auto; object-fit: contain; display: block;" src="' + Lampa.TMDB.image("/t/p/w300" + path.replace(".svg", ".png")) + '" />');
                         imgElement.on('error', function() {
                             $(".full-start-new__title").html(movie.title || movie.name);
